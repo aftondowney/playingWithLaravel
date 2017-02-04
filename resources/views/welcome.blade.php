22362ee75f0c5695f4df22a5512c1354ef9c2806
Afton Downey <aaike001@gmail.com>
2017-02-04 09:33:49 -0800

    create welcome page with sign in form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,44 @@
-@extends('layout')
+@extends('layouts.master')
 
 @section('title')
     Welcome!
+@endsection
+
 @section('content')
+    <div class="row">
+      <div class="col-sm-6">
+        <h3> Sign Up </h3>
+        <form action="#" method="post">
+          <div class="form-group">
+            <label for="email">Your Email</label>
+            <input class="form-control" type="text" name="email" id="email">
+          </div>
+          <div class="form-group">
+            <label for="first_name">First Name</label>
+            <input class="form-control" type="text" name="first_name" id="first_name">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input class="form-control" type="text" name="password" id="password">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
 
-    <h1>The Welcome Page Goes Here</h1>
+      </div>
+      <div class="col-sm-6">
+        <h3> Sign In </h3>
+        <form action="#" method="post">
+          <div class="form-group">
+            <label for="email">Your Email</label>
+            <input class="form-control" type="text" name="email" id="email">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input class="form-control" type="text" name="password" id="password">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
 
-@stop
+      </div>
+    </div>
+@endsection
